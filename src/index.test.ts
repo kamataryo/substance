5c8183e85b336658da7eb759c25963fd5bf597b5
kamataryo <mugil.cephalus+ciconia.local@gmail.com>
2019-03-29 11:02:58 +0900

    Fix some

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,18 @@
 import substance from "./";
 import test from "ava";
 
-test("it should return JSON", async t => {
-  t.deepEqual(await substance({}), {});
+test("it should return JSON", async t => t.deepEqual(await substance({}), {}));
+
+test("it should parse JSON", async t => t.deepEqual(await substance("{}"), {}));
+
+test("it should repace null with undefined", async t =>
+  t.deepEqual(await substance({ a: null, b: 1 }), { b: 1 }));
+
+test("it should replace null deeply", async t =>
+  t.deepEqual(await substance({ a: { b: null }, c: 1 }), { a: {}, c: 1 }));
+
+test("it return error with circular object", t => {
+  const input = { a: { b: 1, x: {} }, c: 2 };
+  input.a.x = input.a;
+  t.throwsAsync(async () => await substance(input));
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-export const substance = (x: object) => x;
+const walker = (obj) => {
+  const keys =
+}
+
+export const substance = (arg: object | string) => {
+  const json = typeof arg === "string" ? JSON.parse(arg) : arg;
+  const result = {}
+  for
+};
 
 export default substance;
